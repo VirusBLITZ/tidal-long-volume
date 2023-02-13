351d49aca3b0179db3bdf6d39410309cde2b8446
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2023-02-13 14:58:59 +0100

    move to variables for future upgrades

--- acc_volume.ts
+++ acc_volume.ts
@@ -1,13 +1,16 @@
+const SLIDER_HEIGHT = 250;
+// const SLIDER_HEIGHT = 800;
+
 const addlistener = () => {
   console.info("Adding listener");
   document.querySelector('button[data-test="volume"]')!.addEventListener("mouseover", () => {
     const nativeRange = document.querySelector<HTMLElement>("div#nativeRange")!;
-    nativeRange.style.height = "265px";
-    nativeRange.style.top = "calc(-100% - 245px)";
+    nativeRange.style.height = `${SLIDER_HEIGHT + 15}px`;
+    nativeRange.style.top = `calc(-100% - ${SLIDER_HEIGHT - 5}px)`;
 
     const volInput = document.querySelector<HTMLInputElement>(".nativeRange--ZV0FD")!;
     if (!volInput) console.error("No volume input found!");
-    volInput.style.minWidth = "250px";
+    volInput.style.minWidth = `${SLIDER_HEIGHT}px`;
   });
 }
 
