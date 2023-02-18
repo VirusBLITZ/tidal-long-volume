6c591e2d87e81395c7007c990bcab1bda1cc09ce
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2023-02-18 22:41:51 +0100

    fix issue where slider would not be detected if loading took too long

--- acc_volume.ts
+++ acc_volume.ts
@@ -1,19 +1,31 @@
 const SLIDER_HEIGHT = 250;
 // const SLIDER_HEIGHT = 800;
+let RETRIES = 10;
 
 const addlistener = () => {
-  console.info("Adding listener");
-  document.querySelector('button[data-test="volume"]')!.addEventListener("mouseover", () => {
+  const volBtn = document.querySelector('button[data-test="volume"]') as HTMLElement;
+  if (!volBtn) return false;
+
+  volBtn.addEventListener("mouseover", () => {
     const nativeRange = document.querySelector<HTMLElement>("div#nativeRange")!;
+    if (!nativeRange) return false;
     nativeRange.style.height = `${SLIDER_HEIGHT + 15}px`;
     nativeRange.style.top = `calc(-100% - ${SLIDER_HEIGHT - 5}px)`;
 
     const volInput = document.querySelector<HTMLInputElement>(".nativeRange--ZV0FD")!;
-    if (!volInput) console.error("No volume input found!");
+    if (!volInput) return false;
     volInput.style.minWidth = `${SLIDER_HEIGHT}px`;
   });
+  return true;
 }
 
-setTimeout(() => {
-  addlistener();
-}, 1000);
+const main = async () => {
+  while (RETRIES > 0) {
+    if (addlistener()) break;
+    RETRIES--;
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
+  if (RETRIES <= 0) console.error("Failed to add listener to volume button.");
+};
+
+main();
